f874755a33734464e8fd2eac911bd8bcfa6bfe0f
Joseph Hirotsu <jhirotsu@gmail.com>
2022-06-21 13:33:53 -0400

    Update joseph.js add fetch

--- joseph.js
+++ joseph.js
@@ -2,39 +2,44 @@
 //2) play/pause function
 //3) skip to next song function
 
-// If play button clicked, change pp.src to pause button and call song.play(). If pause button clicked, change pp.src to play button and call song.pause().
-let playing = true;
-const song = document.querySelector('#idForAudioSrcTag')
-const thumbnail = document.querySelector('#idForAlbumCover')
-const songArtist = document.querySelector('#id')
-const songTitle = document.querySelector('#id')
 
+const songSrc = document.querySelector('#idForAudioSrcTag')
+const songImage = document.querySelector('#idForAlbumCover')
+const bandName = document.querySelector('#id')
+const titleName = document.querySelector('#id')
+
+fetch ('http://localhost:3000/music')
+.then(res=> res.json())
+.then(music => console.log(music))
 songs = music.musicfile
 thumbnails = music.imagefile
 songArtists = music.bandname
 songTitles =music.titlename
 
+// If play button clicked, change pp.src to pause button and call song.play(). If pause button clicked, change pp.src to play button and call song.pause().
+let playing = true
 function playPause() {
   if (playing) {
-    pPause.src = "**pause.png"; //this will change the 
+    pPause.src = "images/pause.png"; //this will change the 
     // Play button to a Pause button
     // thumbnail.style.transform = "scale(1.15)"; //this will slightly 
     // zoom in the album cover for a cool effect
-    song.play(); //this will play the audio track
+    songSrc.play(); //this will play the audio track
     playing = false;
   } else {
     pPause.src = "images/play.png"
     // thumbnail.style.transform = "scale(1)" //this will slightly 
     // zoom in the album cover for a cool effect
  
-    song.pause();
+    songSrc.pause();
     playing = true;
   }
 }
 // automatically play the next song at the end of the audio object's duration
-song.addEventListener('ended', function(){
+songSrc.addEventListener('ended', function(){
     nextSong();
 });
+
 // function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track
 songIndex = 0;
 function nextSong() {
@@ -42,11 +47,11 @@ function nextSong() {
   if (songIndex > 1) {
     songIndex = 0;
   };//change all these variables when html is updated
-  song.src = songs[songIndex];
-  thumbnail.src = thumbnails[songIndex];
+  songSrc.src = songs[songIndex];
+  songImage.src = thumbnails[songIndex];
   background.src = thumbnails[songIndex];
-  songArtist.innerHTML = songArtists[songIndex];
-  songTitle.innerHTML = songTitles[songIndex];
+  bandName.innerHTML = songArtists[songIndex];
+  titleName.innerHTML = songTitles[songIndex];
   playing = true;
   playPause();
 }
@@ -57,11 +62,11 @@ function previousSong() {
     if (songIndex < 0) {
       songIndex = 1;
     };
-    song.src = songs[songIndex];
-    thumbnail.src = thumbnails[songIndex];
+    songSrc.src = songs[songIndex];
+    songImage.src = thumbnails[songIndex];
     background.src = thumbnails[songIndex];
-    songArtist.innerHTML = songArtists[songIndex];
-    songTitle.innerHTML = songTitles[songIndex];
+    bandName.innerHTML = songArtists[songIndex];
+    titleName.innerHTML = songTitles[songIndex];
     playing = true;
     playPause();
   }
